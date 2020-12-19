5ad380657fc6608eaaedf656b350811f7114f20e
martynp <12006448+martynp@users.noreply.github.com>
2020-12-19 07:32:39 +0000

    Cleans up responses.py

--- confrm/responses.py
+++ confrm/responses.py
@@ -1,4 +1,20 @@
-"""Custom FastAPI Response handler for sending data from memory as a file"""
+"""
+Custom FastAPI Response handler for sending data from memory as a file
+
+Copyright 2020 confrm.io
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 from fastapi.responses import Response
 from starlette.types import Receive, Scope, Send
@@ -7,16 +23,12 @@ class ConfrmFileResponse(Response):
     """Response class to enable files to be transfered from memory
 
     Builds on the FileResponse class in starlette.types.
-
-    Attributes:
-        data (bytes): Bytes array containing data to be transfered
-
     """
 
     chunk_size = 4096
 
-    def __init__(self, data: bytes = None) -> None:
-        """ Init method for ConfrmFileResponse class """
+    def __init__(self, data: bytes = None) -> None: #pylint: disable=W0231
+        """Init method for ConfrmFileResponse class """
 
         self.data = data
         self.media_type = "application/octet-stream"
@@ -25,11 +37,10 @@ class ConfrmFileResponse(Response):
         self._headers_set = False
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
-        """ Call method, overrides default method in FileResponse
+        """Call method, overrides default method in FileResponse
 
-        Sets HTTP headers, if not set, and then recursivley calls the send
+        Sets HTTP headers, if not set, and then calls the send
         method to transfer data to TCP stack.
-
         """
 
         if self._headers_set is False:
