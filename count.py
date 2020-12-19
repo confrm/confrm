dd0cbf62cb658329790aa62b9d4b134aa5fcbe7e
martynp <12006448+martynp@users.noreply.github.com>
2020-12-19 07:54:51 +0000

    Removes debug script

--- count.py
+++
@@ -1,8 +0,0 @@
-import struct
-
-with open("blob.bin", "wb") as fp:
-    for i in range(0, 5000):
-        val = i
-        if i == 1000:
-            val = 1001
-        fp.write(struct.pack("=L", val))
